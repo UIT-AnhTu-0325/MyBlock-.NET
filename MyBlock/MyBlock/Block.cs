064dd210304059dba58de6d4373a36c06bc00017
Đặng Anh Tú <danganhtu2001@gmail.com>
2022-02-22 06:45:44 +0700

    Init project

---
+++ MyBlock/MyBlock/Block.cs
@@ -0,0 +1,49 @@
+﻿using Blockchain.First.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBlock
+{
+    internal class Block
+    {
+        public string hash { get; set; }
+        public string previousHash { get; set; }
+        private string data { get; set; }
+        private long timeStamp { get; set; } = 0;
+
+        private int nonce = 0;
+
+
+        public Block(string data, string previousHash)
+        {
+            this.data = data;
+            this.previousHash = previousHash;
+            this.timeStamp = DatetimeHandle.GetTime();
+            this.hash = CalculateHash();
+        }
+        public string CalculateHash()
+        {
+            HashSha256 sha256 = new HashSha256();
+            string calculatedhash = sha256.Hash(
+                previousHash +
+                timeStamp.ToString() +
+                nonce.ToString() +
+                data);
+            return calculatedhash;
+        }
+        public void MineBlock(int difficulty)
+        {
+            string target = new string(new char[difficulty]).Replace('\0', '0'); //Create a string with difficulty * "0" 
+            while (!hash.Substring(0, difficulty).Equals(target))
+            {
+                nonce++;
+                hash = CalculateHash();
+                //Console.WriteLine(hash);
+            }
+            Console.WriteLine("Block Mined!!! : " + hash);
+        }
+    }
+}
---
+++ MyBlock/MyBlock/Program.cs
@@ -0,0 +1,58 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MyBlock;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+class Program
+{
+    public static List<Block> blockchain = new();
+    public static int difficulty = 4;
+
+    static void Main(string[] args)
+    {
+        blockchain.Add(new Block("Hi im the first block", "0"));
+        
+        Console.WriteLine("Trying to Mine block 1... ");
+        blockchain.ElementAt(0).MineBlock(difficulty);
+
+        blockchain.Add(new Block("Yo im the second block", blockchain.ElementAt(blockchain.Count - 1).hash));
+        
+        Console.WriteLine("Trying to Mine block 2... ");
+        blockchain.ElementAt(blockchain.Count - 1).MineBlock(difficulty);
+
+        blockchain.Add(new Block("Hey im the third block", blockchain.ElementAt(blockchain.Count - 1).hash));
+        
+        Console.WriteLine("Trying to Mine block 3... ");
+        blockchain.ElementAt(blockchain.Count - 1).MineBlock(difficulty);
+        
+        Console.WriteLine("\nBlockchain is Valid: " + IsChainValid());
+        
+        string printBlockChain = JsonSerializer.Serialize(blockchain);
+        
+        Console.WriteLine(printBlockChain);
+        Console.ReadLine();
+    }
+    public static bool IsChainValid()
+    {
+        Block currentBlock;
+        Block previousBlock;
+        //loop through blockchain to check hashes:
+        for (int i = 1; i < blockchain.Count; i++)
+        {
+            currentBlock = blockchain.ElementAt(i);
+            previousBlock = blockchain.ElementAt(i - 1);
+            //compare registered hash and calculated hash:
+            if (!currentBlock.hash.Equals(currentBlock.CalculateHash()))
+            {
+                Console.WriteLine("Current Hashes not equal");
+                return false;
+            }
+            //compare previous hash and registered previous hash
+            if (!previousBlock.hash.Equals(currentBlock.previousHash))
+            {
+                Console.WriteLine("Previous Hashes not equal");
+                return false;
+            }
+        }
+        return true;
+    }
+}
